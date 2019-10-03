96639c9ec35ebae4e03c74beb322a4d81e33a7a6
TPYMGK067643\TP <bejmawiktor@gmail.com>
2019-10-03 14:01:37 +0200

    EventManager added.

---
+++ DDD/Events/EventManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DDD.Events
+{
+    [ExcludeFromCodeCoverage]
+    public sealed class EventManager
+    {
+        private static readonly Lazy<EventManager> instance = new Lazy<EventManager>(
+            () => new EventManager());
+
+        public IEventDispatcher EventDispatcher { get; set; }
+
+        public static EventManager Instance => instance.Value;
+
+        private EventManager()
+        {
+        }
+
+        public void Notify<TEvent>(TEvent @event) where TEvent : IEvent
+        {
+            this.EventDispatcher?.Dispatch(@event);
+        }
+    }
+}
