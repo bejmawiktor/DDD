ae9f41cc076b77e728877af4191afd8730ced931
TPYMGK067643\TP <bejmawiktor@gmail.com>
2019-10-03 13:49:55 +0200

    AggregateRoot added.

---
+++ DDD/Model/AggregateRoot.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DDD.Model
+{
+    public abstract class AggregateRoot<TIdentifier> : Aggregate<TIdentifier>
+         where TIdentifier : IEquatable<TIdentifier>
+    {
+        public AggregateRoot(TIdentifier id) : base(id)
+        {
+        }
+    }
+}
