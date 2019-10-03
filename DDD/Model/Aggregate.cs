1f48ff9a84cff4bdd735e928c93e929ad9fec631
TPYMGK067643\TP <bejmawiktor@gmail.com>
2019-10-03 13:46:40 +0200

    Aggregate added.

---
+++ DDD/Model/Aggregate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DDD.Model
+{
+    public abstract class Aggregate<TIdentifier> : Entity<TIdentifier>
+         where TIdentifier : IEquatable<TIdentifier>
+    {
+        public Aggregate(TIdentifier id) : base(id)
+        {
+        }
+    }
+}
