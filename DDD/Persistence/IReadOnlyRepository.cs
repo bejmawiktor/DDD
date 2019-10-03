3820a8ecb856cfb5e4ca1008832263890040f1e6
TPYMGK067643\TP <bejmawiktor@gmail.com>
2019-10-03 14:11:19 +0200

    IReadOnlyRepository added.

---
+++ DDD/Persistence/IReadOnlyRepository.cs
@@ -0,0 +1,15 @@
+﻿using DDD.Model;
+using System;
+using System.Collections.Generic;
+
+namespace DDD.Persistence
+{
+    public interface IReadOnlyRepository<TEntity, TIdentifier>
+        where TEntity : Entity<TIdentifier>
+        where TIdentifier : IEquatable<TIdentifier>
+    {
+        TEntity Get(TIdentifier identifier);
+
+        IEnumerable<TEntity> Get(Pagination pagination = null);
+    }
+}
---
+++ DDD/Persistence/Pagination.cs
@@ -0,0 +1,14 @@
+﻿namespace DDD.Persistence
+{
+    public class Pagination
+    {
+        public uint Page { get; }
+        public uint ItemsPerPage { get; }
+
+        public Pagination(uint page, uint itemsPerPage)
+        {
+            this.Page = page;
+            this.ItemsPerPage = itemsPerPage;
+        }
+    }
+}
