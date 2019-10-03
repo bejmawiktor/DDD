47553e3150fd81db8692ad026e51f577bb15848f
TPYMGK067643\TP <bejmawiktor@gmail.com>
2019-10-03 15:05:20 +0200

    Identifier added.

---
+++ DDD.Tests/Unit/IdentifierTest.cs
@@ -0,0 +1,26 @@
+﻿using DDD.Model;
+using NUnit.Framework;
+using System;
+
+namespace DDD.Tests.Unit
+{
+    [TestFixture]
+    public class IdentifierTest
+    {
+        public class TestId : Identifier<string>
+        {
+            public TestId(string value) : base(value)
+            {
+            }
+        }
+
+        [Test]
+        public void TestIdentifierReferenceTypeValidation()
+        {
+            TestId testId = new TestId("1");
+
+            Assert.Throws<ArgumentNullException>(() => new TestId(null));
+            Assert.That(testId.Value, Is.EqualTo("1"));
+        }
+    }
+}
---
+++ DDD/Model/Identifier.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DDD.Model
+{
+    public abstract class Identifier<TIdentifier> : ValueObject
+        where TIdentifier : IEquatable<TIdentifier>
+    {
+        public TIdentifier Value { get; }
+
+        public Identifier(TIdentifier value)
+        {
+            if(default(TIdentifier) == null && value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            this.Value = value;
+        }
+
+        protected override IEnumerable<object> GetEqualityMembers()
+        {
+            yield return this.Value;
+        }
+    }
+}
