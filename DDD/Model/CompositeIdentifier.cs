1754eb6c21e2a388bc7db57a610b5369af861102
TPYMGK067643\TP <bejmawiktor@gmail.com>
2020-05-05 10:58:37 +0200

    CompositeIdentifier added.

---
+++ DDD/Model/CompositeIdentifier.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace DDD.Model
+{
+    public abstract class CompositeIdentifier<TTupleKeys, TDeriviedCompositeIdentifier>
+    : Identifier<TTupleKeys, TDeriviedCompositeIdentifier>
+        where TTupleKeys : ITuple, IEquatable<TTupleKeys>
+        where TDeriviedCompositeIdentifier : CompositeIdentifier<TTupleKeys, TDeriviedCompositeIdentifier>
+    {
+        protected CompositeIdentifier(TTupleKeys value) : base(value)
+        {
+        }
+    }
+}
