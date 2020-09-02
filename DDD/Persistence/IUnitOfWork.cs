8bad46ddbace5c16274d76e13a5a92db49fbe2b1
TPYMGK067643\TP <bejmawiktor@gmail.com>
2020-09-02 15:53:57 +0200

    Added IUnitOfWork interface.

---
+++ DDD/Persistence/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DDD.Persistence
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        void BeginTransaction();
+
+        void Save();
+
+        void Commit();
+
+        void Rollback();
+    }
+}
