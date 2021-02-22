463fa121d48e72ea45d1f7d8a1a8fbb048156d41
BlekU <bejmawiktor@gmail.com>
2021-02-22 20:09:29 +0100

    Event abstract class added.

---
+++ DDD/Events/Event.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DDD.Events
+{
+    public abstract class Event : IEvent
+    {
+        public Guid Id { get; }
+        public DateTime CreatedAt { get; }
+
+        public Event()
+        {
+            this.Id = Guid.NewGuid();
+            this.CreatedAt = DateTime.Now;
+        }
+    }
+}
