8868c8fa490783427caf29792753df0192bc5bde
bejmawiktor <bejmawiktor@gmail.com>
2021-02-22 13:42:54 +0100

    Pagination tests added.

---
+++ DDD.Tests/Unit/Persistence/PaginationTest.cs
@@ -0,0 +1,25 @@
+﻿using DDD.Persistence;
+using NUnit.Framework;
+
+namespace DDD.Tests.Unit.Persistence
+{
+    [TestFixture]
+    public class PaginationTest
+    {
+        [Test]
+        public void TestConstructing_WhenPageGiven_ThenPageIsSet()
+        {
+            var pagination = new Pagination(10, 1000);
+
+            Assert.That(pagination.Page, Is.EqualTo(10));
+        }
+
+        [Test]
+        public void TestConstructing_WhenItemsPerPageGiven_ThenItemsPerPageIsSet()
+        {
+            var pagination = new Pagination(10, 1000);
+
+            Assert.That(pagination.ItemsPerPage, Is.EqualTo(1000));
+        }
+    }
+}
