c935328bd954781600b1cc1848eab5b5c4024794
bejmawiktor <bejmawiktor@gmail.com>
2021-03-01 13:27:41 +0100

    IAsyncReadOnlyDtoRepository added.

---
+++ DDD/Application/Persistence/IAsyncReadOnlyDtoRepository.cs
@@ -0,0 +1,13 @@
+﻿using DDD.Persistence;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DDD.Application
+{
+    public interface IAsyncReadOnlyDtoRepository<TDto, TDtoIdentifier>
+    {
+        Task<TDto> GetAsync(TDtoIdentifier identifier);
+
+        Task<IEnumerable<TDto>> GetAsync(Pagination pagination = null);
+    }
+}
