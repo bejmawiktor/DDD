5da15f1f0d025cf7fd9a46ceaa798234f4fc8360
bejmawiktor <bejmawiktor@gmail.com>
2021-03-01 13:20:16 +0100

    IWriteOnlyDtoRepository added.

---
+++ DDD/Application/Persistence/IWriteOnlyDtoRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace DDD.Application
+{
+    public interface IWriteOnlyDtoRepository<TDto, TDtoIdentifier>
+    {
+        void Add(TDto entity);
+
+        void Remove(TDto entity);
+
+        void Update(TDto entity);
+    }
+}
