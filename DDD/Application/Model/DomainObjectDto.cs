2f0d38c1570d674b6bdd75cefa616b9af2d0fabc
bejmawiktor <bejmawiktor@gmail.com>
2021-03-01 12:47:35 +0100

    DomainObjectDto added.

---
+++ DDD/Application/Model/DomainObjectDto.cs
@@ -0,0 +1,10 @@
+﻿using DDD.Model;
+
+namespace DDD.Application
+{
+    public abstract class DomainObjectDto<TDomainObject>
+        where TDomainObject : IDomainObject
+    {
+        protected internal abstract TDomainObject ToDomainObject();
+    }
+}
