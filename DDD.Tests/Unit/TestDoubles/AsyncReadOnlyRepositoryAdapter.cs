0d835a81688e1cb4670337c009c00e75967d0c73
bejmawiktor <bejmawiktor@gmail.com>
2021-03-01 13:42:34 +0100

    IAsyncReadOnlyRepositoryAdapter added.

---
+++ DDD.Tests/Unit/TestDoubles/AsyncReadOnlyRepositoryAdapter.cs
@@ -0,0 +1,17 @@
+﻿using DDD.Application;
+
+namespace DDD.Tests.Unit.TestDoubles
+{
+    public class AsyncReadOnlyRepositoryAdapter
+    : IAsyncReadOnlyRepositoryAdapter<AggregateRootDtoStub, string, AsyncAggregateRootDtoStubRORepository, AggregateRootDtoStubConverter, AggregateRootStub, string>,
+        IAsyncAggregateRootStubRORepository
+    {
+        AsyncAggregateRootDtoStubRORepository IAsyncReadOnlyRepositoryAdapter<AggregateRootDtoStub, string, AsyncAggregateRootDtoStubRORepository, AggregateRootDtoStubConverter, AggregateRootStub, string>.DtoRepository
+            => this.DtoRepository;
+
+        public AsyncAggregateRootDtoStubRORepository DtoRepository { get; }
+
+        public AsyncReadOnlyRepositoryAdapter(AsyncAggregateRootDtoStubRORepository dtoRepository)
+            => this.DtoRepository = dtoRepository;
+    }
+}
