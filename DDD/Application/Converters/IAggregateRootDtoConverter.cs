729dba7b367e0aa7b71d10bbfbd3ee9d581462bf
bejmawiktor <bejmawiktor@gmail.com>
2021-03-01 12:51:15 +0100

    IAggregateRootDtoConverter added.

---
+++ DDD/Application/Converters/IAggregateRootDtoConverter.cs
@@ -0,0 +1,15 @@
+﻿using DDD.Model;
+using System;
+
+namespace DDD.Application
+{
+    public interface IAggregateRootDtoConverter<TAggregateRoot, TIdentifier, TDto, TDtoIdentifier>
+        where TAggregateRoot : IAggregateRoot<TIdentifier>
+        where TIdentifier : IEquatable<TIdentifier>
+        where TDto : AggregateRootDto<TAggregateRoot, TIdentifier>
+    {
+        TDto ToDto(TAggregateRoot aggregateRoot);
+
+        TDtoIdentifier ToDtoIdentifier(TIdentifier identifier);
+    }
+}
