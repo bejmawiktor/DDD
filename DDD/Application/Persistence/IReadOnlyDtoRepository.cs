886e5cbf1d9622794691f523e51f1cd8b9f429e6
bejmawiktor <bejmawiktor@gmail.com>
2021-03-01 13:19:52 +0100

    IReadOnlyDtoRepository added.

---
+++ DDD/Application/Persistence/IReadOnlyDtoRepository.cs
@@ -0,0 +1,12 @@
+﻿using DDD.Persistence;
+using System.Collections.Generic;
+
+namespace DDD.Application
+{
+    public interface IReadOnlyDtoRepository<TDto, TDtoIdentifier>
+    {
+        TDto Get(TDtoIdentifier identifier);
+
+        IEnumerable<TDto> Get(Pagination pagination = null);
+    }
+}
