48a703ea5d9ba95e73fd391e2292d397be9986d8
bejmawiktor <bejmawiktor@gmail.com>
2021-03-01 12:50:14 +0100

    AggregateRootDto added.

---
+++ DDD/Application/Model/AggregateRootDto.cs
@@ -0,0 +1,11 @@
+﻿using DDD.Model;
+using System;
+
+namespace DDD.Application
+{
+    public abstract class AggregateRootDto<TAggregateRoot, TIdentifier> : DomainObjectDto<TAggregateRoot>
+        where TAggregateRoot : IAggregateRoot<TIdentifier>
+        where TIdentifier : IEquatable<TIdentifier>
+    {
+    }
+}
