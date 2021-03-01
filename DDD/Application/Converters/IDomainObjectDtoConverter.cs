329fc9ca9f83b65f2b6373271cf2effad06227bb
bejmawiktor <bejmawiktor@gmail.com>
2021-03-01 12:49:45 +0100

    IDomainObjectDtoConverter added.

---
+++ DDD/Application/Converters/IDomainObjectDtoConverter.cs
@@ -0,0 +1,11 @@
+﻿using DDD.Model;
+
+namespace DDD.Application
+{
+    public interface IDomainObjectDtoConverter<TDomainObject, TDto>
+        where TDomainObject : IDomainObject
+        where TDto : DomainObjectDto<TDomainObject>
+    {
+        TDto ToDto(TDomainObject domainObject);
+    }
+}
