e9295adea00a0c105443b289a1878cafb205e974
bejmawiktor <bejmawiktor@gmail.com>
2021-03-01 13:37:40 +0100

    WriteOnlyRepositoryAdapter added.

---
+++ DDD.Tests/Unit/TestDoubles/AggregateRootDtoStubWORepository.cs
@@ -0,0 +1,30 @@
+﻿using DDD.Application;
+using System.Collections.Generic;
+
+namespace DDD.Tests.Unit.TestDoubles
+{
+    public class AggregateRootDtoStubWORepository : IWriteOnlyDtoRepository<AggregateRootDtoStub, string>
+    {
+        public List<AggregateRootDtoStub> Dtos { get; private set; }
+
+        public AggregateRootDtoStubWORepository(List<AggregateRootDtoStub> dtos)
+        {
+            this.Dtos = dtos;
+        }
+
+        public void Add(AggregateRootDtoStub entity)
+        {
+            this.Dtos.Add(entity);
+        }
+
+        public void Remove(AggregateRootDtoStub entity)
+        {
+            this.Dtos.RemoveAt(this.Dtos.FindIndex(e => e.Id == entity.Id));
+        }
+
+        public void Update(AggregateRootDtoStub entity)
+        {
+            this.Dtos[this.Dtos.FindIndex(e => e.Id == entity.Id)] = entity;
+        }
+    }
+}
---
+++ DDD/Application/Persistence/Adapters/IWriteOnlyRepositoryAdapter.cs
@@ -0,0 +1,32 @@
+﻿using DDD.Model;
+using DDD.Persistence;
+using System;
+
+namespace DDD.Application
+{
+    public interface IWriteOnlyRepositoryAdapter<TDto, TDtoIdentifier, TDtoRepository, TDtoAggregateRootConverter, TAggregateRoot, TIdentifier>
+    : IWriteOnlyRepository<TAggregateRoot, TIdentifier>
+        where TDto : AggregateRootDto<TAggregateRoot, TIdentifier>
+        where TDtoRepository : IWriteOnlyDtoRepository<TDto, TDtoIdentifier>
+        where TAggregateRoot : IAggregateRoot<TIdentifier>
+        where TIdentifier : IEquatable<TIdentifier>
+        where TDtoAggregateRootConverter : IAggregateRootDtoConverter<TAggregateRoot, TIdentifier, TDto, TDtoIdentifier>, new()
+    {
+        protected abstract TDtoRepository DtoRepository { get; }
+
+        void IWriteOnlyRepository<TAggregateRoot, TIdentifier>.Add(TAggregateRoot entity)
+        {
+            this.DtoRepository.Add(new TDtoAggregateRootConverter().ToDto(entity));
+        }
+
+        void IWriteOnlyRepository<TAggregateRoot, TIdentifier>.Remove(TAggregateRoot entity)
+        {
+            this.DtoRepository.Remove(new TDtoAggregateRootConverter().ToDto(entity));
+        }
+
+        void IWriteOnlyRepository<TAggregateRoot, TIdentifier>.Update(TAggregateRoot entity)
+        {
+            this.DtoRepository.Update(new TDtoAggregateRootConverter().ToDto(entity));
+        }
+    }
+}
