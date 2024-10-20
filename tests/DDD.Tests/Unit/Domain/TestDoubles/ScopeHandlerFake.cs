ea26eb798f1b837b5841b165109d427748a1ddcf
bejmawiktor <52658106+bejmawiktor@users.noreply.github.com>
2024-10-20 17:42:29 +0200

    feat: Scope and IScopeHandler added

---
+++ tests/DDD.Tests/Unit/Domain/TestDoubles/ScopeHandlerFake.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using DDD.Domain.Common;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace DDD.Tests.Unit.Domain.TestDoubles;
+
+internal class ScopeHandlerFake : IScopeHandler<ScopeFake, string, ScopeHandlerFake>
+{
+    private static readonly Lazy<ScopeHandlerFake> instance = new(() => new ScopeHandlerFake());
+
+    private static readonly AsyncLocal<ScopeFake?> localScope = new();
+
+    static ScopeFake? IScopeHandler<ScopeFake, string, ScopeHandlerFake>.CurrentScope
+    {
+        get => ScopeHandlerFake.CurrentScope;
+        set => ScopeHandlerFake.CurrentScope = value;
+    }
+
+    static ScopeHandlerFake IScopeHandler<ScopeFake, string, ScopeHandlerFake>.Instance =>
+        ScopeHandlerFake.Instance;
+
+    public static ScopeFake? CurrentScope
+    {
+        get => localScope.Value;
+        internal set => localScope.Value = value;
+    }
+
+    public static ScopeHandlerFake Instance => instance.Value;
+
+    public IDispatcher? Dispatcher { get; set; }
+
+    public ScopeHandlerFake() { }
+}
