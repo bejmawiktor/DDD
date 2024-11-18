8b8368ba02b4424603df25bf4f373f09e6aec88d
Wiktor Bejma <bejmawiktor@gmail.com>
2024-11-18 19:29:06 +0100

    feat: Result added to utils.

---
+++ src/DDD.Domain/Utils/IResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DDD.Domain.Utils;
+
+public interface IResult<TError>
+    : IEquatable<Result.SuccessResult>,
+        IEquatable<Result.FailureResult>
+    where TError : IError
+{
+    TError? Error { get; }
+    bool IsSuccess { get; }
+    bool IsFailure { get; }
+}
+
+public interface IResult<TValue, TError> : IResult<TError>
+    where TError : IError
+{
+    TValue? Value { get; }
+}
