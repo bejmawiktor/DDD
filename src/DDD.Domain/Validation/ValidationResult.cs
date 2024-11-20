9bc1cd6fdd0eea755160896b6d698563e1fe93f2
Wiktor Bejma <bejmawiktor@gmail.com>
2024-11-20 22:35:03 +0100

    feat: ValidationResult unnecessary methods remove

--- src/DDD.Domain/Validation/ValidationResult.cs
+++ src/DDD.Domain/Validation/ValidationResult.cs
@@ -13,17 +13,6 @@ public class ValidationResult<TException> : Result<ValidationError<TException>>
         : base(new ValidationError<TException>(exceptions)) { }
 
     internal ValidationResult() { }
-
-    public TMatchResult Match<TMatchResult>(
-        Func<TMatchResult> onSuccess,
-        Func<IEnumerable<TException>, TMatchResult> onFailure
-    )
-    {
-        ArgumentNullException.ThrowIfNull(onSuccess);
-        ArgumentNullException.ThrowIfNull(onFailure);
-
-        return this.IsSuccess ? onSuccess() : onFailure(this.Error?.Reasons!);
-    }
 }
 
 public class ValidationResult<TValue, TException> : Result<TValue, ValidationError<TException>>
@@ -36,23 +25,6 @@ public class ValidationResult<TValue, TException> : Result<TValue, ValidationErr
 
     internal ValidationResult(IEnumerable<TException> exceptions)
         : base(new ValidationError<TException>(exceptions)) { }
-
-    public void Deconstruct(out TValue? value, out IEnumerable<TException>? exceptions)
-    {
-        value = this.Value;
-        exceptions = this.Error?.Reasons;
-    }
-
-    public TMatchResult Match<TMatchResult>(
-        Func<TValue, TMatchResult> onSuccess,
-        Func<IEnumerable<TException>, TMatchResult> onFailure
-    )
-    {
-        ArgumentNullException.ThrowIfNull(onSuccess);
-        ArgumentNullException.ThrowIfNull(onFailure);
-
-        return this.IsSuccess ? onSuccess(this.Value!) : onFailure(this.Error?.Reasons!);
-    }
 
     protected override IEnumerable<object?> GetEqualityMembers()
     {
