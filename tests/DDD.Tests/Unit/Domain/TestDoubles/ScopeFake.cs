68cd3765fb82f63154554ec2e21e7f9283c665ea
Wiktor Bejma <bejmawiktor@gmail.com>
2025-03-27 20:26:02 +0100

    fix: Not used test doubles remove.

--- tests/DDD.Tests/Unit/Domain/TestDoubles/ScopeFake.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-using Utils.Disposable;
-
-namespace DDD.Tests.Unit.Domain.TestDoubles;
-
-internal class ScopeFake : Scope<string, ScopeFake, ScopeHandlerFake>
-{
-    public new IReadOnlyCollection<string> Items => base.Items.AsReadOnly();
-}
