519c165b0d74037672ce62c8052efa0b28963772
Wiktor Bejma <bejmawiktor@gmail.com>
2026-07-17 18:11:10 +0200

    fix: rename.

--- src/DDD.Domain.Events.MediatR/EventManagerExtension.cs
+++ src/DDD.Domain.Events.MediatR/EventManagerExtension.cs
@@ -8,13 +8,13 @@ namespace DDD.Domain.Events.MediatR;
 /// </summary>
 public static class EventManagerExtension
 {
-    extension(EventManager eventManger)
+    extension(EventManager eventManager)
     {
         /// <summary>
         /// Configures the event manager to dispatch domain events through MediatR.
         /// </summary>
         /// <param name="mediator">The MediatR mediator used to publish events.</param>
         public void UseMediatREventDispatcher(IMediator mediator) =>
-            eventManger.Dispatcher = new EventDispatcher(mediator);
+            eventManager.Dispatcher = new EventDispatcher(mediator);
     }
 }
